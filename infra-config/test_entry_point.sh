ebdf49b65e342506122f15a73201b5da1251704e
abdelrahman mohamed <amohamed@localhost.localdomain>
2018-04-01 16:14:11 +0200

    test

--- infra-config/test_entry_point.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-bundle check || bundle install
-bundle exec rake db:create db:schema:load
-
-# We don't want the container to exit to be able to run specs on in it on-demand
-# with docker exec
-
-tee
